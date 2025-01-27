015a57f072d605b346c35415a5b02de5dd120d74
SuhailManzoor <sohailmanzoorsario@gmail.com>
2025-01-27 18:24:51 +0500

    Stack, how stack works in Flutter,commonly used in example You saw a Background or cover photo behind profile picture, this is how stack works first profile picture and then behind cover photo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:seventh_stack_structures/stack_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: home(example),
+      home: StackView(),
     );
     }
 }
